0bfa96785ab1ead5c41c08e33401624feb650035
AlexeyMonkey13 <Aptuct89@inbox.ru>
2023-01-15 23:15:27 +0700

    решение первой задачи

---
+++ seminar4/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+Console.WriteLine("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int thirdpow = a;
+int i = 1;
+
+while (i < b)
+{
+    thirdpow = thirdpow * a;
+    i++;
+}
+Console.WriteLine(thirdpow);
