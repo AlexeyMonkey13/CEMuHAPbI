c91a8656414c6665f47c869767c13ff03c335a5a
AlexeyMonkey13 <Aptuct89@inbox.ru>
2023-01-16 20:01:22 +0700

    решение второй задачи

---
+++ seminar4/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+void SumNumb(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine(result);
+    }
+
+Console.WriteLine("Введите число");
+SumNumb(Convert.ToInt32(Console.ReadLine()));
