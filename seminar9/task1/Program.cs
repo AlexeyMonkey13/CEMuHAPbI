53b2d28078a0536d273127575cc32e1ff8355eae
AlexeyMonkey13 <Aptuct89@inbox.ru>
2023-02-03 15:42:06 +0700

    решение задач семинара 9

---
+++ seminar9/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Clear();
+Console.WriteLine("Введите значение");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 0)
+{
+    Numbers(n, 1);
+}
+if(n < 0)
+{
+    NumbersMinus(1, n);
+}
+
+void Numbers(int start, int end)
+{
+    Console.Write($"{start}");
+    if(start == end) return;
+    Numbers(start - 1, end);
+    
+}
+
+void NumbersMinus(int start, int end)
+{
+    Console.Write($"{start} ");
+    if(start == end) return;
+    Numbers(start- 1, end - 1);
+    
+}
+
---
+++ seminar9/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.WriteLine("Введите min значение");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите max значение");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = SumNum(m, n);
+Console.WriteLine($"сумма значений от min до max = {a}");
+
+int SumNum(int min, int max)
+{
+    int sum = 0;
+    if (min == max)
+    {
+        return min;
+    }
+    sum = min + SumNum(min + 1, max);
+    return sum;
+}
+
